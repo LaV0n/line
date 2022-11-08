ea53c1d413292568785b43547cd112c546fa98d3
LaV0n <leonidmedica@gmail.com>
2022-11-08 10:58:37 +0100

    feat: add bord

--- src/constants/AudioSource.ts
+++ src/constants/AudioSource.ts
@@ -1,4 +1,10 @@
+export const playAudio = (url: string) => {
+    new Audio(url).play()
+}
+
 export const AudioSource={
     coin:'https://assets.mixkit.co/sfx/preview/mixkit-unlock-game-notification-253.mp3',
     win:'https://assets.mixkit.co/sfx/preview/mixkit-game-level-completed-2059.mp3',
+    error:'https://assets.mixkit.co/sfx/preview/mixkit-game-show-wrong-answer-buzz-950.mp3',
+    correct:'https://assets.mixkit.co/sfx/preview/mixkit-player-jumping-in-a-video-game-2043.mp3'
 }
--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -1,60 +1,59 @@
 import styles from './Main.module.scss'
 import {Coin} from "./Coin/Coin";
-import React, {useRef, useState} from 'react';
-import {AudioSource} from "../../constants/AudioSource";
+import React, {useEffect, useRef, useState} from 'react';
+import {AudioSource, playAudio} from "../../constants/AudioSource";
 import arrow from '../../assets/image/right-arrow-svgrepo-com.svg'
 import {NavLink} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../bll/store";
 import {resetInitialCoins} from "../../bll/gameReducer";
-
+import background2 from '../../assets/image/background2.jpg'
+import background3 from '../../assets/image/background3.jpg'
+import {Button} from '@mui/material';
 
 export const Main = () => {
 
     const dragItem: any = useRef();
     const dragDiv: any = useRef();
     const initialCoins = useAppSelector<any[]>(state => state.game.initialCoins)
+    const numberOfItem = useAppSelector(state => state.game.numberOfItem)
+    const [backgroundImage, setBackgroundImage] = useState(background3);
     const dispatch = useAppDispatch()
     const direction = useAppSelector(state => state.game.direction)
-    const kindOfCoin=useAppSelector(state => state.game.kindOfCoin)
-
+    const kindOfCoin = useAppSelector(state => state.game.kindOfCoin)
     const [coins, setCoins] = useState(initialCoins);
-    const [panels, setPanels] = useState<number[] |string[]>([]);
-
-    const playAudio = (url: string) => {
-        new Audio(url).play()
-    }
+    const [panels, setPanels] = useState<number[] | string[]>([]);
 
     const dragStart = (e: any) => {
         dragItem.current = e.target.innerHTML;
-        console.log(e.target.innerHTML);
         playAudio(AudioSource.coin)
     };
 
     const dragEnter = (e: any) => {
         dragDiv.current = e.target.id;
-        console.log(e.target.id);
     };
 
     const drop = () => {
-
-        let itemIndex: number | null=0
+        let itemIndex: number | null = 0
         let conditionUp
         let conditionDown
-        if(kindOfCoin!==26){
+        if (kindOfCoin !== 26) {
             itemIndex = coins.findIndex(f => f === +dragItem.current)
-             conditionUp = (Math.min(...coins ) === +dragItem.current) && (panels.length === 0  ? true : dragItem.current > panels[panels.length - 1])
-             conditionDown = (Math.max(...coins) === +dragItem.current) && (panels.length === 0  ? true : dragItem.current < panels[panels.length - 1])
-        }else{
+            conditionUp = (Math.min(...coins) === +dragItem.current) && (panels.length === 0 ? true : dragItem.current > panels[panels.length - 1])
+            conditionDown = (Math.max(...coins) === +dragItem.current) && (panels.length === 0 ? true : dragItem.current < panels[panels.length - 1])
+        } else {
             itemIndex = coins.findIndex(f => f === dragItem.current)
-            conditionUp = (Math.min(...coins.map(c=>c.charCodeAt(0) - 97)) === dragItem.current.charCodeAt(0) - 97) && (panels.length === 0  ? true : dragItem.current > panels[panels.length - 1])
-            conditionDown = (Math.max(...coins.map(c=>c.charCodeAt(0) - 97)) === dragItem.current.charCodeAt(0) - 97) && (panels.length === 0  ? true : dragItem.current < panels[panels.length - 1])
+            conditionUp = (Math.min(...coins.map(c => c.charCodeAt(0) - 97)) === dragItem.current.charCodeAt(0) - 97) && (panels.length === 0 ? true : dragItem.current > panels[panels.length - 1])
+            conditionDown = (Math.max(...coins.map(c => c.charCodeAt(0) - 97)) === dragItem.current.charCodeAt(0) - 97) && (panels.length === 0 ? true : dragItem.current < panels[panels.length - 1])
         }
 
-        if (itemIndex !== -1 && dragDiv.current === 'panel' && (direction==='up'? conditionUp : conditionDown) ) {
+        if (itemIndex !== -1 && dragDiv.current === 'panel' && (direction === 'up' ? conditionUp : conditionDown)) {
             let dragItemContent = coins[itemIndex];
             setCoins(coins.filter(u => u !== dragItemContent))
             setPanels([...panels, dragItemContent])
             itemIndex = null;
+            playAudio(AudioSource.correct)
+        } else {
+            playAudio(AudioSource.error)
         }
     };
 
@@ -65,14 +64,18 @@ export const Main = () => {
     }
     if (coins.length === 0) playAudio(AudioSource.win)
 
+    useEffect(() => {
+        setBackgroundImage(Math.random() < 0.5 ? background3 : background2)
+    }, [])
+
     return (
         <div>
-            <div className={styles.background}></div>
+            <div className={styles.background} style={{backgroundImage: `url(${backgroundImage})`}}></div>
             {coins.length === 0 &&
                 <div className={styles.winnerBlock}>
                     You Are Winner
-                    <NavLink to={'/'}>
-                        <button onClick={resetGame}>try again</button>
+                    <NavLink to={'/'} style={{textDecoration: 'none'}}>
+                        <Button onClick={resetGame} variant="contained" color={'success'}>try again</Button>
                     </NavLink>
                 </div>
             }
@@ -82,21 +85,25 @@ export const Main = () => {
                         <Coin key={c} dragStart={dragStart} coin={c} drop={drop}/>
                     )}
                 </div>
-                    {direction === 'up'
-                        ? <div className={styles.direction}>
-                            <span>Way Up</span>
-                            <img src={arrow} className={styles.arrow} alt={'0'}/>
-                        </div>
-                        : <div className={styles.direction}>
-                            <span>Way Down</span>
-                            <img src={arrow} className={styles.arrow} alt={'0'} style={{transform:'rotate(0.5turn)'}}/>
-                        </div>
-                    }
+                {direction === 'up'
+                    ? <div className={styles.direction}>
+                        <span>Way Up</span>
+                        <img src={arrow} className={styles.arrow} alt={'0'}/>
+                    </div>
+                    : <div className={styles.direction}>
+                        <span>Way Down</span>
+                        <img src={arrow} className={styles.arrow} alt={'0'} style={{transform: 'rotate(0.5turn)'}}/>
+                    </div>
+                }
                 <div className={styles.panel} id='panel' onDragEnter={dragEnter}>
                     {panels.map((p) =>
                         <Coin coin={p} key={p}/>
                     )}
                 </div>
+                <div className={styles.panelWithHole}>
+                    {[...Array(numberOfItem)].map(p => <div className={styles.hole} key={p}></div>)}
+                </div>
+
             </div>
         </div>
     )
